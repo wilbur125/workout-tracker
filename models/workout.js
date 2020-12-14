bf88c0a6acfa0376e3083a9e8f18370194e03c37
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-13 23:49:39 -0800

    Coded to get total duration or exercises array

--- models/workout.js
+++ models/workout.js
@@ -47,6 +47,14 @@ const workoutSchema = new Schema(
   }
 );
 
+//gets total duration for exercises
+//exercises is an array and reduce() will get the sum of the array at exercises.duration
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+});
+
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;

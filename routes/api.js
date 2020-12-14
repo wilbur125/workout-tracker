858ef320a7b11cf97261b27e0c27f4a364a045f2
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-14 00:08:26 -0800

    add post route for api/workouts to create a new workout

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,14 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 
